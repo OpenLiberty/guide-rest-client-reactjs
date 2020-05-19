ce9d738be2238bfd9b8da12f4221731abeb2f3be
DannyS03 <danny.salman@ryerson.ca>
2020-05-18 20:02:36 -0400

    refactor/simplify backend rest app

--- finish/src/main/java/io/openliberty/guides/consumingrest/ArtistApplication.java
+++ finish/src/main/java/io/openliberty/guides/consumingrest/ArtistApplication.java
@@ -1,6 +1,6 @@
 // tag::comment[]
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,12 +10,12 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
  // end::comment[]
-package io.openliberty.guides.consumingrest.model;
+package io.openliberty.guides.consumingrest;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("")
+@ApplicationPath("artists")
 public class ArtistApplication extends Application {
 
 }
