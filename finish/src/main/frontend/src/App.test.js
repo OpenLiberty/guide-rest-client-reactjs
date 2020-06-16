fff6942ce7a508408f3491a366b978cb3cfecc86
DannyS03 <danny.salman@ryerson.ca>
2020-06-16 10:35:32 -0400

    addressing SME feedback

--- finish/src/main/frontend/src/App.test.js
+++ finish/src/main/frontend/src/App.test.js
@@ -4,6 +4,6 @@ import App from './Components/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Artist Web Service/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = getByText(/Artist Web Service/i);
+  expect(titleElement).toBeInTheDocument();
 });
