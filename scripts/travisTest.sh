79bb9064c1de9aa9536890fcf504a04d08711514
Gilbert Kwan <gkwan@ca.ibm.com>
2020-07-22 17:45:36 -0400

    Update travisTest.sh

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -15,9 +15,9 @@
 #       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
 #       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory.
 mvn -q clean process-resources
-echo $?
+echo process-resources: $?
 mvn -q package liberty:create liberty:install-feature liberty:deploy
-echo $?
+echo install OL: $?
 
 ## Run the tests
 # These commands are separated because if one of the commands fail, the test script will fail and exit.
@@ -27,7 +27,7 @@ echo $?
 #       liberty:stop              - Stop a Liberty server.
 #       failsafe:verify           - Verifies that the integration tests of an application passed.
 mvn liberty:start
-if [ "$?" ne "0" ]; then echo start OK; else echo start NOT OK; exit 1; fi;
+if [ "$?" -ne "0" ]; then echo start Not OK; else echo start OK; exit 1; fi;
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:3000")"; if [ "$status" == "200" ]; then echo ENDPOINT OK; else echo "$status"; echo ENDPOINT NOT OK; exit 1; fi;
 
