cd57ddb9be7bacc666f40a31b4767a6bd3d7368c
Tien-Thanh Le <tienthanh.le@mail.utoronto.ca>
2020-12-02 13:39:23 -0500

    daily build

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 ##############################################################################
 ##
-##  Travis CI test script
+##  GH actions CI test script
 ##
 ##############################################################################
 
@@ -27,6 +27,11 @@ mvn liberty:start
 
 curl http://localhost:9080
 
-status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:9080")"; if [ "$status" == "200" ]; then echo ENDPOINT OK; else echo "$status"; echo ENDPOINT NOT OK; exit 1; fi;
+status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:9080")"
+if [ "$status" == "200" ]; then echo ENDPOINT OK; else
+    echo "$status"
+    echo ENDPOINT NOT OK
+    exit 1
+fi
 
 mvn liberty:stop
