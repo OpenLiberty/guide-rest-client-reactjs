d96a3ddbfbe60aebb648819c7885834a31cfc784
jakub-pomykala <56261244+jakub-pomykala@users.noreply.github.com>
2023-02-07 13:44:52 -0500

    Updating react-table to v7  (#122)
    * Initial updates for react-table-v7
    
    * Removing processed resources
    
    * Update of LMP and react-table + changes to guide readme
    
    * deleting package-lock.json
    
    * removing the webapp dir
    
    * Updating package-lock.json
    
    * Updating webapp dir
    
    * Updating copyright
    
    * Updated LICENSE
    
    * Updating 2022 to 2023
    
    * Fixing double http in license link
    
    * Updating copyright to pass check-files test
    
    * Added table.css to start dir
    
    * Updating css to remove clone code
    
    * Changes requested by Gilbert
    
    * Changes requested by Gilbert
    
    * Changes requested by Gilbert
    
    * Fixing broken hotspot
    
    * Fixing image
    
    * fixing the convert-data hotspot text
    
    * update a paragraph
    
    * update a paragraph
    
    * update a paragraph
    
    * update seo description
    
    * refactor the package.json
    
    * Update README.adoc
    
    Co-authored-by: David Mueller <48686014+dmuelle@users.noreply.github.com>
    
    * Update README.adoc
    
    Co-authored-by: David Mueller <48686014+dmuelle@users.noreply.github.com>
    
    * Update README.adoc
    
    Co-authored-by: David Mueller <48686014+dmuelle@users.noreply.github.com>
    
    * fix a typo
    
    ---------
    
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>
    Co-authored-by: David Mueller <48686014+dmuelle@users.noreply.github.com>

--- finish/src/main/frontend/src/Components/ArtistTable.js
+++ finish/src/main/frontend/src/Components/ArtistTable.js
@@ -1,131 +1,211 @@
 // tag::react-library[]
-import React, { Component } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 // end::react-library[]
 // tag::axios-library[]
 import axios from 'axios';
 // end::axios-library[]
 // tag::react-table[]
-import ReactTable from 'react-table-6';
-import 'react-table-6/react-table.css';
+import { useTable, usePagination, useSortBy } from 'react-table';
 // end::react-table[]
+// tag::custom-style[]
+import '../Styles/table.css'
+// end::custom-styly[]
 
-// tag::class[]
-class ArtistTable extends Component {
-// end::class[]
-  // tag::state-object[]
-  state = {
-    posts: [],
-    isLoading: true,
-    error: null,
-  };
-  // end::state-object[]
+// tag::ArtistTable[]
+export function ArtistTable() {
+  // end::ArtistTable[]
+
+  // tag::posts[]
+  const [posts, setPosts] = useState([]);
+  // end::posts[]
 
   // tag::get-posts[]
-  getArtistsInfo() {
+  const GetArtistsInfo = async () => {
     // tag::axios[]
-    axios('http://localhost:9080/artists')
-    // end::axios[]
-      // tag::then-method[]
+    const response = await axios.get('http://localhost:9080/artists')
+      // end::axios[]
       .then(response => {
         // tag::response-data[]
         const artists = response.data;
         // end::response-data[]
-        // tag::convert-data[]
-        const posts = [];
+        // tag::for-artists[]
         for (const artist of artists) {
           // tag::spread-one[]
           const { albums, ...rest } = artist;
           // end::spread-one[]
           for (const album of albums) {
+            //tag::setState[]
+            setPosts([...posts, { ...rest, ...album }]);
+            // end::setState[]
             // tag::spread-two[]
             posts.push({ ...rest, ...album });
             // end::spread-two[]
           }
         };
-        // end::convert-data[]
-        // tag::set-state[]
-        this.setState({
-          // tag::data[]
-          posts,
-          // end::data[]
-          isLoading: false
-        });
-        // end::set-state[]
-      // end::then-method[]
-      })
-      .catch(error => this.setState({ error, isLoading: false }));
-  }
+        // end::for-artists[]
+      }).catch(error => { console.log(error); });
+  };
   // end::get-posts[]
 
-  // tag::mount-posts[]
-  componentDidMount() {
-    this.getArtistsInfo();
+  // tag::useMemo[]
+  const data = useMemo(() => [...posts], [posts]);
+  // end::useMemo[]
+
+  // tag::table-info[]
+  const columns = useMemo(() => [{
+    Header: 'Artist Info',
+    columns: [
+      {
+        Header: 'Artist ID',
+        accessor: 'id'
+      },
+      {
+        Header: 'Artist Name',
+        accessor: 'name'
+      },
+      {
+        Header: 'Genres',
+        accessor: 'genres',
+      }
+    ]
+  },
+  {
+    Header: 'Albums',
+    columns: [
+      {
+        Header: 'Number of Tracks',
+        accessor: 'ntracks',
+      },
+      {
+        Header: 'Title',
+        accessor: 'title',
+      }
+    ]
   }
-  // end::mount-posts[]
-  // tag::render-posts[]
-  render() {
-    const { isLoading, posts } = this.state;
-    // tag::table-info[]
-    const columns = [{
-      Header: 'Artist Info',
-      columns: [
-        {
-          Header: 'Artist ID',
-          accessor: 'id'
-        },
-        {
-          Header: 'Artist Name',
-          accessor: 'name'
-        },
-        {
-          Header: 'Genres',
-          accessor: 'genres',
-        }
-      ]
-    },
-    {
-      Header: 'Albums',
-      columns: [
-        {
-          Header: 'Title',
-          accessor: 'title',
-        },
-        {
-          Header: 'Number of Tracks',
-          accessor: 'ntracks',
-        }
-      ]
-    }
-  ]
+  ], []
+  );
   // end::table-info[]
+
+  // tag::useTable[]
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 4 }
+    },
+    // tag::useSortBy[]
+    useSortBy,
+    // end::useSortBy[]
+    // tag::usePagination[]
+    usePagination
+    // end::usePagination[]
+  )
+  // end::useTable[]
+
+  // tag::destructuring[]
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize }
+  } = tableInstance;
+  // end::destructuring[]
+
+  // tag::useEffect[]
+  useEffect(() => {
+    GetArtistsInfo();
+  }, []);
+  // end::useEffect[]
 
   // tag::return-table[]
   return (
-    <div>
+    <>
       <h2>Artist Web Service</h2>
-      {!isLoading ? (
-        // tag::table[]
-        <ReactTable
-          // tag::prop-posts[]
-          data={posts}
-          // end::prop-posts[]
-          // tag::prop-columns[]
-          columns={columns}
-          // end::prop-columns[]
-          defaultPageSize={4}
-          pageSizeOptions={[4, 5, 6]}
-        />) : (
-        // end::table[]
-          <p>Loading .....</p>
-        )}
-    </div>
-    );
-    // end::return-table[]
-  }
-  // end::render-posts[]
+      {/* tag::table[] */}
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      {/* end::table[] */}
+      <div className="pagination">
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'Previous'}
+        </button>{' '}
+        <div class="page-info">
+          <span>
+            Page{' '}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{' '}
+          </span>
+          <span>
+            | Go to page:{' '}
+            <input
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={e => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                gotoPage(page)
+              }}
+              style={{ width: '100px' }}
+            />
+          </span>{' '}
+          <select
+            value={pageSize}
+            onChange={e => {
+              setPageSize(Number(e.target.value))
+            }}
+          >
+            {[4, 5, 6, 9].map(pageSize => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'Next'}
+        </button>{' '}
+      </div>
+    </>
+  );
+  // end::return-table[]
 }
-// end::element[]
-
-// tag::export-artisttable[]
-export default ArtistTable;
-// end::export-artisttable[]
