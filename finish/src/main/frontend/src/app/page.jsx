aa8fda149ff8376ab936c2c97f88135be14c71e0
Ruilin Ma <this.is.millerma@gmail.com>
2024-07-02 15:19:47 -0400

    revise content for using Next.js

--- finish/src/main/frontend/src/app/page.jsx
+++ finish/src/main/frontend/src/app/page.jsx
@@ -1,9 +1,13 @@
-import styles from "../../styles/index.css";
+import "../../styles/index.css";
 import ArtistTable from "./ArtistTable";
 import React from 'react';
 
+// tag::createHome[]
 export default function Home() {
   return (
+    // tag::displayArtistTable[]
     <ArtistTable></ArtistTable>
+    // end::displayArtistTable[]
   );
 }
+// end::createHome[]
