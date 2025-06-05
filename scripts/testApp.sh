e43bebda4d7d3882de7781a67ae1bc0476daab6e
Dev Agarwal <124221073+dev0508@users.noreply.github.com>
2025-06-05 17:26:49 -0400

    Update to use maven wrapper (#272)
    * Bump vite from 5.4.18 to 6.3.5 in /finish/src/main/frontend
    
    Bumps [vite](https://github.com/vitejs/vite/tree/HEAD/packages/vite) from 5.4.18 to 6.3.5.
    - [Release notes](https://github.com/vitejs/vite/releases)
    - [Changelog](https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md)
    - [Commits](https://github.com/vitejs/vite/commits/v6.3.5/packages/vite)
    
    ---
    updated-dependencies:
    - dependency-name: vite
      dependency-version: 6.3.5
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump next from 14.2.21 to 14.2.26 in /finish/src/main/frontend
    
    Bumps [next](https://github.com/vercel/next.js) from 14.2.21 to 14.2.26.
    - [Release notes](https://github.com/vercel/next.js/releases)
    - [Changelog](https://github.com/vercel/next.js/blob/canary/release.js)
    - [Commits](https://github.com/vercel/next.js/compare/v14.2.21...v14.2.26)
    
    ---
    updated-dependencies:
    - dependency-name: next
      dependency-version: 14.2.26
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update to use maven wrapper
    
    Signed-off-by: Dev Agarwal <devagarwal0508@gmail.com>
    
    * Update test.yml
    
    * Bump org.junit.jupiter:junit-jupiter from 5.12.2 to 5.13.0 in /finish
    
    Bumps [org.junit.jupiter:junit-jupiter](https://github.com/junit-team/junit5) from 5.12.2 to 5.13.0.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.12.2...r5.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter
      dependency-version: 5.13.0
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.junit.jupiter:junit-jupiter from 5.12.2 to 5.13.0 in /start
    
    Bumps [org.junit.jupiter:junit-jupiter](https://github.com/junit-team/junit5) from 5.12.2 to 5.13.0.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.12.2...r5.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter
      dependency-version: 5.13.0
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Dev Agarwal <devagarwal0508@gmail.com>
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
+./mvnw -version
 
 ##############################################################################
 ##
@@ -10,14 +11,14 @@ set -euxo pipefail
 # LMP 3.0+ goals are listed here: https://github.com/OpenLiberty/ci.maven#goals
 
 export CI=false
-mvn -ntp clean process-resources
+./mvnw -ntp clean process-resources
 
 ## Rebuild the application
 #       package                   - Take the compiled code and package it in its distributable format.
 #       liberty:create            - Create a Liberty server.
 #       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
 #       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory.
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q package liberty:create liberty:install-feature liberty:deploy
@@ -29,7 +30,7 @@ mvn -ntp -Dhttp.keepAlive=false \
 #       failsafe:integration-test - Runs the integration tests of an application.
 #       liberty:stop              - Stop a Liberty server.
 #       failsafe:verify           - Verifies that the integration tests of an application passed.
-mvn -ntp liberty:start
+./mvnw -ntp liberty:start
 
 curl http://localhost:9080
 
@@ -40,4 +41,4 @@ if [ "$status" == "200" ]; then echo ENDPOINT OK; else
     exit 1
 fi
 
-mvn -ntp liberty:stop
+./mvnw -ntp liberty:stop
